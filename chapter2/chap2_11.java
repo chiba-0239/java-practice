92e6d92163b5234a66655189fef66bc0c16f33c0
Yuuta Chiba <dirac0239@gmail.com>
2026-07-10 00:00:35 +0900

    feat: Chapter2 変数の学習

--- chapter2/chap2_11.java
+++ chapter2/chap2_11.java
@@ -1,9 +1,9 @@
-package chapter2;
+package chapter2;//クラスを整理するための住所のようなもの
 
 public class chap2_11 {
   public static void main(String[]args) {
     int a = 10;
-    int b = 5;
+    int b = 5;//5.0はdouble型なので、変数bもdouble型で宣言する必要があります。計算結果も小数を含めて計算されます。
     System.out.println(a / b);
   }
 }
